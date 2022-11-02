d29a25bf35bcd598c2635b3be0fbf3bb6132a699
Jorge Assis <jorgeassis@Jorges-Mac-Pro.local>
2022-11-02 11:23:52 +0000

    Up.

--- Sessions [P]/challengeSolutions/challengesS1.R
+++ Sessions [P]/challengeSolutions/challengesS1.R
@@ -3,6 +3,7 @@
 # Challenge 1.1
 # The main objective of this challenge is to **install** a set of key R packages.
 
+# install packages
 install.packages("devtools", dependencies=TRUE)
 install.packages("raster", dependencies=TRUE)
 install.packages("sp", dependencies=TRUE)
@@ -17,3 +18,12 @@ install.packages("rgdal", dependencies=TRUE)
 # ----------------------------------------------------
 # Challenge 1.2
 # The main objective of this challenge is to **import** a data table in R.
+
+# set the working directory
+setwd("/Volumes/Jellyfish/Dropbox/Tutoring/Classes & Courses/Modelling the distribution of biodiversity and climate change/Git/courseMarAfrica/")
+
+# reading data frame from semicolon delimited files
+myDF <- read.table("Data/dataBases/Paramuricea_clavata.csv", sep =";", header=T)
+
+# view the data.frame in RStudio
+View(myDF)
